30f62f0ddc69f6d1007a8d67dda9d750bea684cc
Lucas Daniele <lucas.daniele@globant.com>
2013-04-24 15:32:00 -0300

    practico_2 24/04/13

--- scripts.js
+++ scripts.js
@@ -7,7 +7,6 @@ $(document).ready(function() {
         var name = $("#alias").val();
         var $div = $('<div />').appendTo('.container');
         $div.attr('id', 'newDiv');
-
         $.ajax({
           url: "http://bootcamp.aws.af.cm/welcome/"+name,
           type: 'GET',
@@ -36,28 +35,38 @@ $(document).ready(function() {
           type: 'GET',
           dataType: 'jsonp',
           context: document.body,
+          beforeSend: function ()
+                    {
+                    $('.modal').show();
+                    $('body').keydown(function(e){
+                        if(e.which == 27){
+                            $('.modal').hide();
+                        }
+                      });
+                    },
           success: function(data) {
-            $('.background').show();
+          $('.background').show();
             var results = data.results;
             var $list = $('#tweetsList');
             var tweet_data;
-
+            tweet_data = " ";
             for (var i = 0; i < results.length; i++) {
-              tweet_data = "<li class='tweet'>"+ "<strong class='tweetId'>" + i + "</strong>";
-              tweet_data += " <span>‏ </span>" ;
-              tweet_data += "<strong class='from_user'>" + results[i].from_user + "</strong>";
-              tweet_data += "<p class='text'> " + results[i].text + "</p>";
-              tweet_data += "<span class='text'> " + results[i].created_at + "</span>";
-              tweet_data += "<img class='profile_image_url' src= " + results[i].profile_image_url +">" + "</li>";
-              $list.append(tweet_data);
-
+              tweet_data +="<div class='col-set' data-role='collapsible' data-collapsed='true'>";
+              tweet_data +="<h3>";
+              tweet_data +="<img src='"+ results[i].profile_image_url +"' class='img-set'/>";
+              tweet_data +="<div class='col-text'>" + results[i].from_user + "</div>";
+              tweet_data +="<span class='ui-li-count ui-btn-up-c ui-btn-corner-all comment-count'>"+ i +"</span>";
+              tweet_data +="</h3>";
+              tweet_data +="<p> " + results[i].text + results[i].created_at + " </p>";
+              tweet_data +="</div>";
             }
-
+            $list.append(tweet_data).trigger('create');
 
           },
           error: function() {
             console.log("An error occurred in the service call");
           }
         })
+
     }
 });
